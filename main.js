4c19a176aaad75ef565992608fa027b98c23cebd
Emilly Cavalcante <emilly.menezescs@gmail.com>
2024-11-23 19:04:52 -0300

    feat: implement all project's pages

---
+++ main.js
@@ -0,0 +1,7 @@
+function filterDropdownYear() {
+    document.getElementById("dropdownContentYears").classList.toggle("show");
+}
+
+function filterDropdownCompanies() {
+    document.getElementById("dropdownContentCompanies").classList.toggle("show");
+}
